1fc05c7eadd3d49550bc01aa90c3aaaf45f4bd49
Samantha <samanthavillafana8@gmail.com>
2024-08-31 19:59:30 -0400

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,19 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        if not self.root:
+            return None
+        
+        nodes_to_visit = [self.root]
+        
+        while nodes_to_visit:
+            node = nodes_to_visit.pop(0)
+            
+            if node['id'] == id:
+                return node
+            
+            nodes_to_visit = node.get('children', []) + nodes_to_visit
+        
+        return None
